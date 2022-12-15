9078ab24e7ca707619b12afa8619f5a688498949
gioinn <gioinn@viettel.com.vn>
2022-12-15 18:28:51 +0700

    init prj

---
+++ src/test/java/prj/selenium/phptravels/common/SetupDriver.java
@@ -0,0 +1,26 @@
+package prj.selenium.phptravels.common;
+
+import java.util.Locale;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import prj.selenium.helpers.PropertiesHelper;
+
+public class SetupDriver {
+  private WebDriver driver;
+
+  public void setDriver(){
+    switch (PropertiesHelper.getValue("brower").toLowerCase(Locale.ROOT)) {
+      case "chrome":
+        driver = initChromeDriver();
+        break;
+      default:
+        System.out.println("Browser " + PropertiesHelper.getValue("browser") + "is invalid. Launching Chrome ...");
+        driver = initChromeDriver();
+    }
+  }
+  public WebDriver initChromeDriver() {
+    System.setProperty("webdriver.chrome.driver", "./driver_chrome/chromedriver.exe");
+    return new ChromeDriver();
+  }
+
+}
